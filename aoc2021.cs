006f0f62ddd224f26185167a2d9e660b1b37fe1c
yugecin <yugecin@users.noreply.github.com>
2021-12-13 09:10:17 +0100

    update the aoc2021.exe runner program
    it now:
    - runs the solution only twice, to save time while testing
    - accepts /BENCH as argument to run the solution 500 times to benchmark
    - loads aoc.dll early with /DEBUG, for easier debugger attaching
    - uses Read instead of ReadKey, since alt+tabbing also triggers ReadKey

--- aoc2021.cs
+++ aoc2021.cs
@@ -6,21 +6,36 @@ namespace aoc2021
 {
 	class Aoc2021
 	{
-		[DllImport("aoc.dll", CallingConvention = CallingConvention.StdCall)]
+		[DllImport("aoc.dll")]
 		public static extern int aoc();
+		[DllImport("kernel32.dll", CallingConvention = CallingConvention.StdCall)]
+		public static extern int LoadLibraryA(string lpLibFileName);
 
 		public static void Main(string[] args)
 		{
-			if (args.Length > 0 && args[0].ToUpper() == "/DEBUG") {
-				Console.WriteLine("aoc2021.exe: /DEBUG present: press a key to continue");
-				Console.ReadKey(true);
+			bool debug = args.Length > 0 && args[0].ToUpper() == "/DEBUG";
+			bool bench = args.Length > 0 && args[0].ToUpper() == "/BENCH";
+			if (debug) {
+				// The module doesn't get loaded until the "aoc" function is called the
+				// first time, and if it's not loaded you can't set a breakpoint in that
+				// function (unless you're using IDA, but I'd like to use CheatEngine as
+				// debugger too). So load the library now, so we can set a breakpoint
+				// while this runner program is waiting for input.
+				LoadLibraryA("aoc.dll");
+				Console.WriteLine("aoc2021.exe: /DEBUG present: press enter to continue");
+				Console.Read();
 			}
 			// see https://docs.microsoft.com/en-us/dotnet/api/system.diagnostics.stopwatch
 			long nanosPerTick = (1000L * 1000L * 1000L) / Stopwatch.Frequency;
 			long min = long.MaxValue, max = long.MinValue, total = 0;
 			int res = aoc();
+			if (debug) {
+				return;
+			}
+			// always do at least one extra iteration, to check if the result is always the same
+			int iterations = bench ? 500 : 1;
 			Stopwatch sw = new Stopwatch();
-			for (int i = 0; i < 500; i++) {
+			for (int i = 0; i < iterations; i++) {
 				sw.Start();
 				int newres = aoc();
 				sw.Stop();
@@ -36,6 +51,15 @@ namespace aoc2021
 					return;
 				}
 				res = newres;
+			}
+			if (!bench) {
+				Console.WriteLine(
+					"{0} {1:F0}ticks/{2:F0}ns timer resolution {3}ns",
+					res,
+					total / 500f, total / 500f * nanosPerTick,
+					nanosPerTick
+				);
+				return;
 			}
 			Console.WriteLine(
 				"{0} avg {1:F0}ticks/{2:F0}ns lo {3}/{4}ns hi {5}/{6}ns timer resolution {7}ns",
--- readme.txt
+++ readme.txt
@@ -4,11 +4,15 @@ I used:
 nasm: NASM version 2.15.05 compiled on Aug 28 2020
 link: Microsoft (R) Incremental Linker Version 8.00.50727.42 (from VC2005)
 (VCS2015 to compile aoc2021.cs)
+debugger: IDA freeware and/or CheatEngine
 
 run "build.bat" and it compiles and spouts all solutions
 
 run "build.bat /DEBUG" to make aoc2021.exe wait before running each solution,
 this is useful to attach a debugger to the waiting process to debug the dll.
+
+run "build.bat /BENCH" to make aoc2021.exe run the solutions multiple times,
+to get an average timing over 500 iterations.
 
 every solution makes a dll with exported function "aoc"
 aoc2021.exe just calls that while having a Stopwatch running
