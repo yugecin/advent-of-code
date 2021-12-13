d6e7769dd55e3215ccc8dc8bbcbfc5274158164f
yugecin <yugecin@users.noreply.github.com>
2021-12-13 13:09:41 +0100

    build.bat: only call vcvars32 if not already done
    Otherwise it will error eventually because of too long input lines
    (because it'll keep appending to PATH).

--- build.bat
+++ build.bat
@@ -1,8 +1,13 @@
 :: any given first argument will be passed to aoc2021.exe
 @ECHO OFF
+REM don't call vcvars32 if it was already called in this session
+REM otherwise it will error eventually because of too long input lines (because it appends to PATH)
+IF "%VSINSTALLDIR%" NEQ "" GOTO:SKIPVCVARS
 CALL "C:\Users\Robin\broftware\Microsoft Visual Studio 8\VC\bin\vcvars32.bat"
 REM my vcvars returns errorlevel 9009 (attempted to execute a non-existing command), so skipping next check
 ::IF %ERRORLEVEL% NEQ 0 GOTO:ERR
+
+:SKIPVCVARS
 CALL :RUN day01a.asm %1 || GOTO:ERR
 CALL :RUN day01b.asm %1 || GOTO:ERR
 CALL :RUN day02a.asm %1 || GOTO:ERR
