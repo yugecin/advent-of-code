40167e8530fe6bb3c01f20bc545263e6b2e7dfb7
yugecin <yugecin@users.noreply.github.com>
2022-12-02 21:14:31 +0100

    2022 day01b

---
+++ 2022/day01b.asm
@@ -0,0 +1,61 @@
+section .text
+; If not using this name, my link.exe gives an unsuppressible warning that
+; "entrypoint is not __stdcall with 12 bytes of args".
+global __DllMainCRTStartup@12
+__DllMainCRTStartup@12:
+	mov eax, 1
+	retn 0Ch
+
+global aoc
+aoc:
+	push esi
+	push ebx
+	push edx
+	push ebp
+
+	xor ecx, ecx
+	xor ebx, ebx
+	xor edx, edx
+	xor ebp, ebp
+	xor esi, esi
+	xor eax, eax
+@add:
+	add ecx, esi
+	mov esi, [input+eax*4]
+@next_elf:
+	inc eax
+	test esi, esi
+	jnz @add
+	cmp ecx, ebx
+	jl @lower
+	mov ebx, ecx
+	cmp ebx, edx
+	jl @lower
+	mov ecx, ebx
+	mov ebx, edx
+	mov edx, ecx
+	cmp edx, ebp
+	jl @lower
+	mov ecx, edx
+	mov edx, ebp
+	mov ebp, ecx
+@lower:
+	xor ecx, ecx
+	mov esi, [input+eax*4]
+	test esi, esi
+	jnz @next_elf
+
+end:
+
+	mov eax, ebx
+	add eax, edx
+	add eax, ebp
+	xor edx, edx ; 64bit return value
+	pop ebp
+	pop edx
+	pop ebx
+	pop esi
+	ret
+
+section .data
+%include "day01input.asm"
